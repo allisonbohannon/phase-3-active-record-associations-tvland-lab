b45edfb866155df8b6530a3fff6275247bded19d
Allison Bohannon <akboh.github@gmail.com>
2022-11-11 13:38:57 -0500

    commit tree 403cd32147b2482f31c7ba05f6f9133236b9ec1f

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        arr = []
+        self.characters.each do |char|
+            item = "#{char.name} - #{char.show.name}"
+            arr << item
+        end
+        arr
+    end
+
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        arr = []
+        self.characters.each do |char|
+            arr << char.actor.full_name
+        end
+        arr
+    end
 end
